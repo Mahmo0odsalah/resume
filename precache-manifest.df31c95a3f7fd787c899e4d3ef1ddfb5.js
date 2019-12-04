4767edb05e0b870537208c9e8e93b00b33692bff
Mahmood <medoysf@gmail.com>
2019-12-04 17:57:51 +0200

    Updates

---
+++ precache-manifest.df31c95a3f7fd787c899e4d3ef1ddfb5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c921cbee0b2e8de4a36dbf8f5813f10d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4d9790b9ca67c572c885",
+    "url": "/static/css/main.d827c825.chunk.css"
+  },
+  {
+    "revision": "c273307a23a4eff5031d",
+    "url": "/static/js/2.8f514b03.chunk.js"
+  },
+  {
+    "revision": "4d9790b9ca67c572c885",
+    "url": "/static/js/main.7eeaec12.chunk.js"
+  },
+  {
+    "revision": "1fbbd938f2fdcf8b1579",
+    "url": "/static/js/runtime-main.87d397d6.js"
+  },
+  {
+    "revision": "95abb2094467e6b6e7041b004fd3521a",
+    "url": "/static/media/agora.95abb209.png"
+  },
+  {
+    "revision": "c304dd57edef92a2dcb80fb69416d98d",
+    "url": "/static/media/background-landing-page.c304dd57.jpg"
+  },
+  {
+    "revision": "e137c57033e6435203d7589784274ed4",
+    "url": "/static/media/sokhna3.e137c570.jpg"
+  }
+]);
